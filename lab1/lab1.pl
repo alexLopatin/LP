0cfe8c0783b285d131321ae445336f8e98248a4a
alexLopatin <minelabory@gmail.com>
2020-03-04 00:44:09 +0300

    added standard functions

--- lab1/lab1.pl
+++ lab1/lab1.pl
@@ -2,6 +2,23 @@ print_list([X|Y]):-
     write(X), write(' '), print_list(Y).
 print_list(_).
 
+length([], 0).
+length([_|L], N):-length(L, M), N is M + 1.
+
+member(X, [X|_]).
+member(X, [_|T]):-member(X, T).
+
+append([], L, L).
+append([X|L1], L2, [X|L3]):-append(L1, L2, L3).
+
+remove(X, [X|T], T).
+remove(X, [Y|T], [Y|Z]):-remove(X, T, Z).
+
+permute([], []).
+permute(L, [X|T]):-remove(X, L, Y), permute(Y, T).
+
+sublist(S, L):-append(_, L1, L), append(S, _, L1).
+
 reverse([],Z,Z).
  reverse([H|T],Z,Acc) :- reverse(T,Z,[H|Acc]).
 
