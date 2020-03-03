476611bb4b94693e20a0e057c4143123660b17c3
alexLopatin <minelabory@gmail.com>
2020-03-03 23:44:30 +0300

    correct pow calc but unordered

--- lab4/lab4.pl
+++ lab4/lab4.pl
@@ -12,11 +12,11 @@ calc_zero([X, Y, Z|T], C):-
     res(Y), R is X - Z, calc_zero([R|T], C).
 
 calc_pows(L, [X,Y,Z|T], C):-
-    pow(Y), pow(X, Z, R), append(L, [R], L1), calc_pows(L1, [T], C).
+    pow(Y), pow(X, Z, R), calc_pows(L, [R|T], C).
 calc_pows(L, [X, Y, Z|T], C):-
     append(L, [X, Y], L1), calc_pows(L1, [Z|T], C).
 calc_pows(L, [X], L1):- 
-    write(X),append(L, [X], L1).
+	append(L, [X], L1).
 
 
 pow(X, 1, X).
